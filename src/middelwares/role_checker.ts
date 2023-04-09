e2f38e31f73f8bf646c8014013e5b5b6fb86be96
Anderson Macias <anderk222u@gmail.com>
2023-04-09 13:32:13 -0500

    role

--- src/middelwares/role_checker.ts
+++ src/middelwares/role_checker.ts
@@ -6,16 +6,15 @@ export function has_role(...roles: Role[]) {
 
     return async function (req: Req, res: ResError, next: Next) {
 
-        return next(); 
  
-     /*    const authorization = req.get('user-id');
+     const authorization = req.get('user-id');
  
         if (!authorization ) return res.status(400)
             .json({ message: `nececitas poner el header user-id con el id del usuario` });
 
         const id = parseInt(authorization);  
 
-        if (!authorization || isNaN(id)) return res.status(400)
+        if (!id || id < 0) return res.status(400)
             .json({ message: `el id del usuario debe ser un numero y mayor a cero` });
 
         const user = await prisma.user.findFirst({ where: { id } });
@@ -29,7 +28,6 @@ export function has_role(...roles: Role[]) {
             .json({ message: `No tienes acceso` });
 
         next();
-*/ 
     } 
 
 };
